8d0a45ea3a0779c41d9b6b29a6a6faaa821141b0
Tanzilu <akmaltanzilu@gmail.com>
2021-04-08 19:32:04 +0700

    update userController

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel")
+const Unit = require("../models/unitModel")
 
 exports.createNewUser = (req, res) => {
     let newUser = new User(req.body);
@@ -6,6 +7,12 @@ exports.createNewUser = (req, res) => {
         if (err) {
             res.status(500).send(err)
         }
-        res.status(201).json(user)
+        let newUnit = new Unit({user_id: user._id, unit: 0})
+        newUnit.save((err_unit, unit) => {
+            if (err_unit) {
+                res.status(500).send(err_unit)
+            }
+            res.status(200).json(user)
+        })
     })
 }
